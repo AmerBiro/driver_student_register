b14422fa597b5c8f1aef86f39513e39d5bfa573e
AmerBiro <amer.biro@gmail.com>
2020-12-30 20:18:24 +0100

    Adding viewpager2

---
+++ app/src/main/java/com/example/driverstudentregister/viewpager/ViewPagerViwer.java
@@ -0,0 +1,64 @@
+package com.example.driverstudentregister.viewpager;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.driverstudentregister.R;
+import com.example.driverstudentregister.databinding.ViewPagerViwerBinding;
+
+
+public class ViewPagerViwer extends Fragment {
+
+    private @NonNull ViewPagerViwerBinding binding;
+    private Info info;
+    private Payment payment;
+    private Theory theory;
+    private Practise practise;
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = ViewPagerViwerBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+
+        info = new Info();
+        payment = new Payment();
+        theory = new Theory();
+        practise = new Practise();
+
+        binding.tabLayut.setupWithViewPager(binding.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager(), 0);
+
+
+        adapter.addFragment(info, "info");
+        adapter.addFragment(payment, "payment");
+        adapter.addFragment(theory, "theory");
+        adapter.addFragment(practise, "practise");
+
+        binding.viewPager.setAdapter(adapter);
+        binding.viewPager.setCurrentItem(1);
+        binding.tabLayut.getTabAt(0).setIcon(R.drawable.ic_baseline_info_24);
+        binding.tabLayut.getTabAt(1).setIcon(R.drawable.ic_baseline_payment_24);
+        binding.tabLayut.getTabAt(2).setIcon(R.drawable.ic_baseline_menu_book_24);
+        binding.tabLayut.getTabAt(3).setIcon(R.drawable.ic_baseline_directions_car_24);
+
+
+        return view;
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
